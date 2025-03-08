6d821ab65d78be17ce8164355b20e9ab2242a5a2
Kevin Martinez <kevinvr@hotmail>
2025-03-08 04:40:59 +0000

    #1 Fixed compile-opencv script

--- userspace/compile-opencv.sh
+++ userspace/compile-opencv.sh
@@ -1,20 +1,24 @@
 #!/bin/bash -e
 
 apt-get update && apt-get install -yq --no-install-recommends \
+    cmake \
     libgstreamer1.0-dev \
     libgstreamer-plugins-base1.0-dev \
     gstreamer1.0-plugins-ugly \
     gstreamer1.0-rtsp \
+    python3 \
+    python3-dev \
     python3-numpy
 
 cd /tmp
-git clone --depth=1 -b 4.5.4 https://github.com/opencv/opencv
+git clone --depth=1 -b 4.11.0 https://github.com/opencv/opencv
 
 cd opencv
 
 mkdir build
 cd build
 
-cmake -D CMAKE_INSTALL_PREFIX=/usr -D WITH_GSTREAMER=ON -D BUILD_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF ..
-make -j$(nproc)
-make install
+CXXFLAGS="-fPIC -O2" cmake -D CMAKE_INSTALL_PREFIX=/usr -D WITH_GSTREAMER=ON -D BUILD_EXAMPLES=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D CMAKE_TOOLCHAIN_FILE=../platforms/linux/aarch64-gnu.toolchain.cmake ..
+CXXFLAGS="-fPIC -O2" make -j$(nproc)
+checkinstall -yD --install=no --fstrans=no --pkgname=opencv
+mv opencv*.deb /tmp/opencv.deb
